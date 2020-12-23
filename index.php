c03d2170622f7c9b7345583ed4c89be930f0ddbe
Zanoni Luiz <zanoniluiz@gmail.com>
2020-12-23 11:17:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 echo "<h1>Openshift Workshop v2.0</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 echo "<br><hr>";
+echo "Workshop Openshift Detran";
 /*echo "<h2>Cidades cadastradas no Banco de Dados:</h2>";
 $conn = new mysqli("mysql", "redhat", "redhat", "workshop");
 if ($conn->connect_error) {
